c016bcabb235254effacc776701425209de25b14
Zamir Narro <josue.narro.a@gmail.com>
2025-09-24 10:02:00 -0500

    feat: update asset paths and add logo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
