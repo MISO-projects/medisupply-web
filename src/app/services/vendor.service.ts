84be6938f3855b60bd6a23d9e04a2ef688a1f576
Zamir Narro <josue.narro.a@gmail.com>
2025-10-10 21:59:24 -0500

    fix: correct import path for environment configuration

--- src/app/services/vendor.service.ts
+++ src/app/services/vendor.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Vendor, VendorResponse } from '../models/vendor.model';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
