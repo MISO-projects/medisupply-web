00f490ea437d3d2b5ce31dde804c29b2549aefd1
Zamir Narro <josue.narro.a@gmail.com>
2025-10-10 21:34:17 -0500

    feat: enhance data table component with pagination

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -1,5 +1,6 @@
-import { Component, input } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { Component, input, viewChild, effect } from '@angular/core';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
 
 export interface TableColumn {
   key: string;
@@ -9,13 +10,31 @@ export interface TableColumn {
 
 @Component({
   selector: 'app-data-table',
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatPaginatorModule],
   templateUrl: './data-table.component.html',
   styleUrl: './data-table.component.css',
 })
 export class DataTableComponent {
   data = input.required<any[]>();
   columns = input.required<TableColumn[]>();
+
+  dataSource = new MatTableDataSource<any>([]);
+  paginator = viewChild.required(MatPaginator);
+
+  constructor() {
+    // Actualizar dataSource cuando cambie data
+    effect(() => {
+      this.dataSource.data = this.data();
+    });
+
+    // Conectar paginator cuando esté disponible
+    effect(() => {
+      const paginatorInstance = this.paginator();
+      if (paginatorInstance) {
+        this.dataSource.paginator = paginatorInstance;
+      }
+    });
+  }
 
   get displayedColumns(): string[] {
     return this.columns().map(col => col.key);
