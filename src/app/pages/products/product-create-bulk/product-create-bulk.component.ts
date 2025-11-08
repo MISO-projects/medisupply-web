f49cdf59909f5a64ed754d3a7424ba334ded01ad
Alejandro Peña <ja.penat1@uniandes.edu.co>
2025-11-07 20:30:07 -0500

    Add button for massive products upload

---
+++ src/app/pages/products/product-create-bulk/product-create-bulk.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-create-bulk',
+  templateUrl: './product-create-bulk.component.html',
+  styleUrls: ['./product-create-bulk.component.css'],
+})
+export class ProductCreateBulkComponent implements OnInit{
+  constructor() {}
+
+  ngOnInit() {}
+}
